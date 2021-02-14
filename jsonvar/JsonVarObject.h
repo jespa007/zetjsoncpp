655f90966f7ccf207b14e4113e02e0ce400bf915
Jordi E <jordi@braingaze.eu>
2021-02-14 21:56:07 +0100

    last changes

--- jsonvar/JsonVarObject.h
+++ jsonvar/JsonVarObject.h
@@ -24,10 +24,6 @@ namespace zetjsoncpp{
 			this->__js_ptr_data_start__ = ptr;
 			this->__js_ptr_data_end__ = ((JsonVar *)&__zj_var_end__ - 1);
 
-			//-------
-			// Iterate on all its elements ...
-			char *aux_p = (char *)this->__js_ptr_data_start__;
-			char *end_p = (char *)this->__js_ptr_data_end__;
 		}
 
 		JsonVarObject(...) {
